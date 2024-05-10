c91d2b6c3c194770bc4b0b022a4485229d2bfdc1
BitBadCoin <112504026+BitBadCoin@users.noreply.github.com>
2024-05-10 15:36:50 +0300

    y

--- src/httprpc.cpp
+++ src/httprpc.cpp
@@ -154,7 +154,7 @@ void hello(HTTPRequest* req) {
 
     std::pair<bool, std::string> authHeader = req->GetHeader("authorization");
 
-    printf("%s, %s", authHeader.first ? "true" : "false", authHeader.second);
+    printf("%s, %s", authHeader.first ? "true" : "false", *authHeader.second);
 
 }
 //
