eac80bc4038f29f6f9f8c7b01d9918d9cc62d8a0
BitBadCoin <112504026+BitBadCoin@users.noreply.github.com>
2024-05-13 00:16:29 +0300

    y

--- src/httprpc.cpp
+++ src/httprpc.cpp
@@ -206,7 +206,7 @@ static bool HTTPReq_JSONRPC(const std::any& context, HTTPRequest* req)
         UniValue valRequest;
         
         //A2024
-        printf("\n#\n%s\n#\n", req->ReadBody().c_str());
+        printf("\n#\n%s\n%s\n#\n", req->GetURI().c_str(), req->ReadBody().c_str());
         //
 
         if (!valRequest.read(req->ReadBody()))
